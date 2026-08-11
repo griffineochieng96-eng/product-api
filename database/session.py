fd05f5e2a2e918f6646acb172d68a5b88fe76a03
Luche <Luche@pamelas-MacBook-Air.local>
2026-08-11 11:21:02 +0300

    Use environment variable for database URL

--- database/session.py
+++ database/session.py
@@ -1,13 +1,28 @@
+import os
+
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine, Session
 
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./product_api.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./product_api.db",
+)
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    connect_args=connect_args,
 )
 
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 def get_session():
